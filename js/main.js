4bd98da45728f6d18a093eafaa05dc38a08c0ec3
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-05-23 19:04:03 +0200

    snake in movement - setinterval OK

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector(".grid");
 
+// //FUNCTION START THE GAME
+
 class GameBoard {
   constructor(width, height) {
     this.width = width;
@@ -32,55 +34,91 @@ const board = new GameBoard(10, 10);
 
 class Snake {
   constructor() {
-    this.initialPosition = 55;
+    this.snakePositions = [55, 56, 57];
+    this.snakeHeadPosition = this.snakePositions.slice(-1)[0];
+    this.direction = "right";
   }
 
   drawSnake() {
-    board.allCells[this.initialPosition].classList.add("snake");
+    this.snakePositions.forEach((index) => {
+      board.allCells[index].classList.add("snake");
+    });
   }
 
   hideLastPosition() {
-      
+    board.allCells[this.snakePositions[0]].classList.remove("snake");
+    this.snakePositions.shift();
   }
 
-  move(direction) {
-    if (direction === "right") {
-      this.initialPosition += 1;
+  move() {
+    console.log(this.direction);
+    if (this.direction === "right") {
+      this.snakeHeadPosition += 1;
+      this.snakePositions.push(this.snakeHeadPosition);
       this.drawSnake();
-    } else if (direction === "left") {
-      this.initialPosition -= 1;
+      this.hideLastPosition();
+    } else if (this.direction === "left") {
+      this.snakeHeadPosition -= 1;
+      this.snakePositions.push(this.snakeHeadPosition);
       this.drawSnake();
-    } else if (direction === "up") {
-      this.initialPosition -= board.width;
+      this.hideLastPosition();
+    } else if (this.direction === "up") {
+      this.snakeHeadPosition -= board.width;
+      this.snakePositions.push(this.snakeHeadPosition);
       this.drawSnake();
-    } else if (direction === "down") {
-      this.initialPosition += board.width;
+      this.hideLastPosition();
+    } else if (this.direction === "down") {
+      this.snakeHeadPosition += board.width;
+      this.snakePositions.push(this.snakeHeadPosition);
       this.drawSnake();
+      this.hideLastPosition();
     }
   }
 }
 
 const snake = new Snake();
-snake.drawSnake();
+// snake.drawSnake() -> When start the game it needs to appears.
 
 window.addEventListener("keydown", (e) => {
+  // IL FAUT ICI MODIFIER POUR ENVOYER L'INFO DANS LE SETINTERVAL
   switch (e.code) {
     case "ArrowLeft":
-      console.log("left");
-      snake.move("left");
+      snake.direction = "left";
       break;
     case "ArrowRight":
-      snake.move("right");
+      snake.direction = "right";
       break;
     case "ArrowUp":
-      snake.move("up");
+      snake.direction = "up";
       break;
     case "ArrowDown":
-      snake.move("down");
+      snake.direction = "down";
       break;
+    default:
+      snake.direction = "left";
   }
 });
 
+// if (currentPosition % gridWidth === gridWidth - 1) {
+//     break}
+
 //   // Set a direction
 //   // Snake should not able able to go right or left if the direction is right or left
+//
 //   // Snake expension
+
+// FOR TOMORROW
+// 1. Make the snake move alone
+// 2. Get the food position randomly
+// 3. When the snake eats the food -> Expend + 1 && recall the random function
+// 4. Set a un score and increment it when the food is eaten.
+
+// 11:00 -> https://www.youtube.com/watch?v=rui2tRRVtc0 ALL CONDITIONS
+// https://www.youtube.com/watch?v=TAmYp4jKWoM
+// https://www.youtube.com/watch?v=QTcIXok9wNY
+
+const intervalId = setInterval(() => {
+    snake.move()
+}, 1000);
+
+function gameOver() {}
